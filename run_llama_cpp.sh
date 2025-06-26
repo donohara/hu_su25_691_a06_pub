1902fa1e1e37c00c19bce35f05046f1a83c9b47d
Don O'Hara <don.ohara@gmail.com>
2025-06-26 04:02:47 -0400

    initial

--- run_llama_cpp.sh
+++ run_llama_cpp.sh
@@ -1,14 +1,11 @@
-
-#MODEL_DIR="/Users/donohara/.lmstudio/models/RichardErkhov/distilbert_-_distilgpt2-gguf"
-#MODEL_NAME="distilgpt2.Q4_0.gguf"
-
+# Location and name of GGUF model for llama.cpp
 MODEL_DIR="/Users/donohara/.lmstudio/models/lmstudio-community/DeepSeek-R1-Distill-Qwen-7B-GGUF"
 MODEL_NAME="DeepSeek-R1-Distill-Qwen-7B-Q3_K_L.gguf"
 
+# Location of llama.cpp binary
 LLAMA_CPP="/opt/homebrew/Cellar/llama.cpp/5740/bin/llama-server"
 
-#CMD="$LLAMA_CPP --model $MODEL_DIR/$MODEL_NAME --prompt \"Hello, world!\" --n-predict 100 --ctx_size 256 --threads 1"
+# Final command to start the llama.cpp server
 CMD="$LLAMA_CPP --model $MODEL_DIR/$MODEL_NAME --n-predict 1024 --ctx_size 4096 --threads 32"
-
 echo $CMD
 eval $CMD%
